c808c0075dbf6e4d1f64ac01b2daf26256dd5041
4tex <yash.agrawal0305@gmail.com>
2021-11-10 23:20:58 +0530

    bubble sort implementation && breaking the script

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // console.log("script added");
 
-
 let nums = [];
 let canCreate = 1;
 
@@ -12,6 +11,7 @@ function between(min , max){
 }
 
 async function createArray(){
+
 
     nums = [];
     for( let i = 0 ; i<100 ; i++){
@@ -46,3 +46,41 @@ create.addEventListener("click" , async function(){
     }
 
 });
+
+let bubble = document.getElementById("bubble");
+
+bubble.addEventListener("click", async function(){
+
+    // console.log("clicking");
+    let arena = document.querySelector(".arena");
+    let children = arena.children;
+
+    console.log(children);
+
+    let left = children[0].style.height;
+
+    for(let i = 0; i<100; i++){
+
+        for( let j = i ; j< 100 - i - 1; j++){
+
+            
+            let right = children[j+1].style.height;
+
+            if(i ===0 && j === 0){
+                console.log(left);
+            }
+
+            children[j].style.height = left;
+
+            // if(left > right){
+
+            //     children[j].style.height = right;
+            //     children[j+1].style.heigt = left;
+
+            // }
+
+        }
+
+    }
+
+})
